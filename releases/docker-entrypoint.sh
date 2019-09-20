b13ab4f4cc604d5f335eb9a58813f6b4c405da71
Kayuii <577738@qq.com>
2019-09-20 16:09:07 +0800

    fix some bug with docker-entrypoint.sh

--- releases/docker-entrypoint.sh
+++ releases/docker-entrypoint.sh
@@ -7,7 +7,34 @@ if [ $(echo "$1" | cut -c1) = "-" ]; then
 fi
 
 if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "blocknetdxd" ]; then
+  mkdir -p "$BITCOIN_DATA"
+  
+  if [ ! -s "$BITCOIN_DATA/blocknetdx.conf" ]; then
+    cat <<EOF > "$BITCOIN_DATA/blocknetdx.conf"
+printtoconsole=1
+rpcallowip=::/0
+rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
+rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+datadir=$BITCOIN_DATA 
+dbcache=256 
+maxmempool=512 
+port=7721 
+rpcport=7722 
+listen=1 
+server=1 
+logtimestamps=1 
+logips=1 
+rpcthreads=8 
+rpctimeout=15 
+rpcclienttimeout=15 
+EOF
+    chown bitcoin:bitcoin "$BITCOIN_DATA/blocknetdx.conf"
+  fi
+
   chown -R bitcoin:bitcoin "$BITCOIN_DATA"
+  ln -sfn "$BITCOIN_DATA" /home/bitcoin/.blocknetdx 
+	chown -h bitcoin:bitcoin /home/bitcoin/.blocknetdx 
+
   echo "$0: setting data directory to $BITCOIN_DATA"
   set -- "$@" -datadir="$BITCOIN_DATA"
 fi
