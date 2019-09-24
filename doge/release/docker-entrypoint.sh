c2fff31841b7131db95a96c1218940afd0ba49bd
Kayuii <577738@qq.com>
2019-09-24 18:40:29 +0800

    add doge

---
+++ doge/release/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+if [[ "$1" == "dogecoin-cli" || "$1" == "dogecoin-tx" || "$1" == "dogecoind" || "$1" == "test_dogecoin" ]]; then
+	mkdir -p "$BITCOIN_DATA"
+
+	if [[ ! -s "$BITCOIN_DATA/dogecoin.conf" ]]; then
+		cat <<-EOF > "$BITCOIN_DATA/dogecoin.conf"
+printtoconsole=1
+rpcallowip=::/0
+rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
+rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+EOF
+		chown bitcoin:bitcoin "$BITCOIN_DATA/dogecoin.conf"
+	fi
+
+	# ensure correct ownership and linking of data directory
+	# we do not update group ownership here, in case users want to mount
+	# a host directory and still retain access to it
+	chown -R bitcoin "$BITCOIN_DATA"
+	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.dogecoin
+	chown -h bitcoin:bitcoin /home/bitcoin/.dogecoin
+
+	exec gosu bitcoin "$@"
+fi
+
+exec "$@"
