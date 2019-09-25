0bb52eac2c6a49bbfbbb9e90c63e5fc46b47c5b9
Kayuii <577738@qq.com>
2019-09-25 13:47:55 +0800

    add pivx

--- release/docker-entrypoint.sh
+++ release/docker-entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" == "dogecoin-cli" || "$1" == "dogecoin-tx" || "$1" == "dogecoind" || "$1" == "test_dogecoin" ]]; then
+if [[ "$1" == "pivx-cli" || "$1" == "pivx-tx" || "$1" == "pivxd" || "$1" == "test_pivx" ]]; then
 	mkdir -p "$BITCOIN_DATA"
 
-	if [[ ! -s "$BITCOIN_DATA/dogecoin.conf" ]]; then
-		cat <<-EOF > "$BITCOIN_DATA/dogecoin.conf"
+	if [[ ! -s "$BITCOIN_DATA/pivx.conf" ]]; then
+		cat <<-EOF > "$BITCOIN_DATA/pivx.conf"
 printtoconsole=1
 rpcallowip=::/0
 rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 rpcuser=${BITCOIN_RPC_USER:-bitcoin}
 EOF
-		chown bitcoin:bitcoin "$BITCOIN_DATA/dogecoin.conf"
+		chown bitcoin:bitcoin "$BITCOIN_DATA/pivx.conf"
 	fi
 
 	# ensure correct ownership and linking of data directory
 	# we do not update group ownership here, in case users want to mount
 	# a host directory and still retain access to it
 	chown -R bitcoin "$BITCOIN_DATA"
-	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.dogecoin
-	chown -h bitcoin:bitcoin /home/bitcoin/.dogecoin
+	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.pivx
+	chown -h bitcoin:bitcoin /home/bitcoin/.pivx
 
 	exec gosu bitcoin "$@"
 fi
