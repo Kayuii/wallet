7a058a18fe95e5eec697acc805e55b1aeb75caa1
Kayuii <577738@qq.com>
2019-09-25 14:21:10 +0800

    branch for dash

--- release/docker-entrypoint.sh
+++ release/docker-entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" == "pivx-cli" || "$1" == "pivx-tx" || "$1" == "pivxd" || "$1" == "test_pivx" ]]; then
+if [[ "$1" == "dash-cli" || "$1" == "dash-tx" || "$1" == "dashd" || "$1" == "test_dash" ]]; then
 	mkdir -p "$BITCOIN_DATA"
 
-	if [[ ! -s "$BITCOIN_DATA/pivx.conf" ]]; then
-		cat <<-EOF > "$BITCOIN_DATA/pivx.conf"
+	if [[ ! -s "$BITCOIN_DATA/dash.conf" ]]; then
+		cat <<-EOF > "$BITCOIN_DATA/dash.conf"
 printtoconsole=1
 rpcallowip=::/0
 rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 rpcuser=${BITCOIN_RPC_USER:-bitcoin}
 EOF
-		chown bitcoin:bitcoin "$BITCOIN_DATA/pivx.conf"
+		chown bitcoin:bitcoin "$BITCOIN_DATA/dash.conf"
 	fi
 
 	# ensure correct ownership and linking of data directory
 	# we do not update group ownership here, in case users want to mount
 	# a host directory and still retain access to it
 	chown -R bitcoin "$BITCOIN_DATA"
-	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.pivx
-	chown -h bitcoin:bitcoin /home/bitcoin/.pivx
+	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.dash
+	chown -h bitcoin:bitcoin /home/bitcoin/.dash
 
 	exec gosu bitcoin "$@"
 fi
