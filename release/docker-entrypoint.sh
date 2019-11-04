dc3d511b7dc7603acbbc88ea7d4ec881eab9e993
Kayuii <577738@qq.com>
2019-11-04 16:58:17 +0800

    add streamitcoin

--- release/docker-entrypoint.sh
+++ release/docker-entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" == "pivx-cli" || "$1" == "pivx-tx" || "$1" == "pivxd" || "$1" == "test_pivx" ]]; then
+if [[ "$1" == "streamitcoin-cli" || "$1" == "streamitcoin-tx" || "$1" == "streamitcoind" ]]; then
 	mkdir -p "$BITCOIN_DATA"
 
-	if [[ ! -s "$BITCOIN_DATA/pivx.conf" ]]; then
-		cat <<-EOF > "$BITCOIN_DATA/pivx.conf"
+	if [[ ! -s "$BITCOIN_DATA/streamitcoin.conf" ]]; then
+		cat <<-EOF > "$BITCOIN_DATA/streamitcoin.conf"
 printtoconsole=1
 rpcallowip=::/0
 rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 rpcuser=${BITCOIN_RPC_USER:-bitcoin}
 EOF
-		chown bitcoin:bitcoin "$BITCOIN_DATA/pivx.conf"
+		chown bitcoin:bitcoin "$BITCOIN_DATA/streamitcoin.conf"
 	fi
 
 	# ensure correct ownership and linking of data directory
 	# we do not update group ownership here, in case users want to mount
 	# a host directory and still retain access to it
 	chown -R bitcoin "$BITCOIN_DATA"
-	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.pivx
-	chown -h bitcoin:bitcoin /home/bitcoin/.pivx
+	ln -sfn "$BITCOIN_DATA" /home/bitcoin/.streamitcoin
+	chown -h bitcoin:bitcoin /home/bitcoin/.streamitcoin
 
 	exec gosu bitcoin "$@"
 fi
