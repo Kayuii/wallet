2fe75c43f0b2b8b4ae02e3dc0cdf7e14f0b61e19
gecko1 <gecko@yjy.com>
2020-01-05 12:40:54 +0800

    add dccy docker images

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # wallet
+DCCY Installation tutorial
+
+https://github.com/DCCY-Group/DCCY-Deploy.git
+
+https://github.com/ichuan/dccy-docker
---
+++ enable_history.js
@@ -0,0 +1,13 @@
+module.exports = function(fibos) {
+    let accountName = process.env.WATCH_ACCOUNT;
+    if (accountName) {
+      fibos.load('history', {
+        'filter-on': [accountName + ':transfer:', accountName + ':extransfer:'],
+      });
+  
+      fibos.load('history_api');
+    }
+  
+    fibos.start();
+  };
+  
