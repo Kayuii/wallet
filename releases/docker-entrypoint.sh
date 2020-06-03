1623e89d496c3f347e98d2703cac434db7cefaff
gecko1 <gecko@yjy.com>
2020-06-03 12:53:34 +0800

    update dapp docker images version

--- releases/docker-entrypoint.sh
+++ releases/docker-entrypoint.sh
@@ -2,15 +2,15 @@
 set -e
 
 if [ $(echo "$1" | cut -c1) = "-" ]; then
-  echo "$0: assuming arguments for dappercoind"
-  set -- dappercoind "$@"
+  echo "$0: assuming arguments for dappcoind"
+  set -- dappcoind "$@"
 fi
 
-if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "dappercoind" ]; then
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "dappcoind" ]; then
   mkdir -p "$BITCOIN_DATA"
   
-  if [ ! -s "$BITCOIN_DATA/dappercoin.conf" ]; then
-    cat <<EOF > "$BITCOIN_DATA/dappercoin.conf"
+  if [ ! -s "$BITCOIN_DATA/dappcoin.conf" ]; then
+    cat <<EOF > "$BITCOIN_DATA/dappcoin.conf"
 printtoconsole=1
 rpcallowip=::/0
 rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
@@ -29,18 +29,18 @@ rpcthreads=8
 rpctimeout=15 
 rpcclienttimeout=15 
 EOF
-    chown bitcoin:bitcoin "$BITCOIN_DATA/dappercoin.conf"
+    chown bitcoin:bitcoin "$BITCOIN_DATA/dappcoin.conf"
   fi
 
   chown -R bitcoin:bitcoin "$BITCOIN_DATA"
-  ln -sfn "$BITCOIN_DATA" /home/bitcoin/.dappercoin 
-	chown -h bitcoin:bitcoin /home/bitcoin/.dappercoin 
+  ln -sfn "$BITCOIN_DATA" /home/bitcoin/.dappcoin 
+	chown -h bitcoin:bitcoin /home/bitcoin/.dappcoin 
 
   echo "$0: setting data directory to $BITCOIN_DATA"
   set -- "$@" -datadir="$BITCOIN_DATA"
 fi
 
-if [ "$1" = "dappercoind" ] || [ "$1" = "dappercoin-cli" ] || [ "$1" = "dappercoin-tx" ]; then
+if [ "$1" = "dappcoind" ] || [ "$1" = "dappcoin-cli" ] || [ "$1" = "dappcoin-tx" ]; then
   echo "run : $@ "
   exec gosu bitcoin "$@"
 fi
