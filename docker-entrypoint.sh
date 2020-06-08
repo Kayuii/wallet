3b816a1ec349b188872f7a289d03cc09222d9abd
gecko1 <gecko@yjy.com>
2020-06-08 13:50:56 +0800

    update dev

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -28,6 +28,13 @@ logips=1
 rpcthreads=8 
 rpctimeout=15 
 rpcclienttimeout=15 
+addnode=209.97.131.147
+addnode=209.97.139.20
+addnode=209.97.139.2
+addnode=206.189.155.48
+addnode=167.99.234.81
+addnode=138.197.146.236
+addnode=209.97.131.20
 EOF
     chown bitcoin:bitcoin "$BITCOIN_DATA/deviant.conf"
   fi
